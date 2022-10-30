1d5e2ef78db05f183cfe2e665500226abcdb58e7
arabicman888 <8345807@qq.com>
2022-10-30 19:39:41 -0400

    Daily commit

---
+++ src/test/java/com/junxiao/stream/Employee.java
@@ -0,0 +1,24 @@
+package com.junxiao.stream;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    int id;
+    String name;
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
--- src/test/java/com/junxiao/stream/StreamTest.java
+++ src/test/java/com/junxiao/stream/StreamTest.java
@@ -1,15 +1,26 @@
 package com.junxiao.stream;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.OptionalInt;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class StreamTest {
 
+    List<Employee> empList = null;
+
+    @Before
+    public void init()  {
+        Employee emp1 = new Employee(1, "Jack");
+        Employee emp2 = new Employee(2, "Mary");
+        Employee emp3 = new Employee(3, "Rose");
+        empList = Arrays.asList(emp1, emp2, emp3);
+    }
+
     @Test
-    public void test() {
+    public void mathOpTest() {
 
         //1. Math Operation
         int[] numbers = {4, 6, 13, 90, 16, 2, 0};
@@ -18,10 +29,73 @@ public class StreamTest {
         //Max -> OptionalInt
         System.out.println(Arrays.stream(numbers).max().getAsInt());
         //Average ->OptionalDouble
-        System.out.println(Arrays.stream(numbers).average().getAsDouble());
+        Arrays.stream(numbers).average().getAsDouble();
         //Count -> long
-        System.out.println(IntStream.of(numbers).count());
+        IntStream.of(numbers).count();
         //Sum -> int
-        System.out.println(IntStream.of(numbers).sum());
+        IntStream.of(numbers).sum();
+
+        //2.IntSummaryStatistics
+        IntSummaryStatistics statistics = IntStream.of(numbers).summaryStatistics();
+        statistics.getMin();
+        statistics.getMax();
+        statistics.getAverage();
+        statistics.getCount();
+        statistics.getSum();//return long
+
+    }
+
+
+    @Test
+    public void streamProcessTest() {
+        int[] numbers = {4, 6, 13, 90, 16, 2, 0};
+        //3. Stream Operations  (create stream -> process stream -> consume stream)
+        //get the 3 smallest distinct number
+        IntStream.of(numbers)  //create stream
+                .distinct()
+                .sorted()
+                .limit(3)
+                .forEach(System.out::println); //consume stream
+
+        //4. create stream
+        IntStream.of(numbers);
+        IntStream.range(1, 101);
+        IntStream.rangeClosed(1, 100);
+        IntStream.generate(() -> (int)(Math.random() * 100)).limit(10).forEach(System.out::println);
+
+        IntStream intStream = IntStream.of(numbers);
+
+        //5. process stream
+        intStream.distinct();                   //distinct
+        intStream.sorted();                     //sort
+        intStream.limit(3);             //get first 3
+        intStream.skip(3);                   //skip first 3
+        intStream.filter(num -> num % 2 == 0);  //only even number
+        intStream.map(num -> num *2);           //double each value
+        intStream.boxed();                      //convert Int to Integer
+        intStream.mapToObj(i -> new Employee(i, "Emp"+i)); //convert to Object
+
+        //6. consume stream
+        intStream.average();
+        intStream.min();
+        intStream.max();
+        intStream.sum();
+        intStream.count();
+        intStream.forEach(System.out::println);
+        intStream.toArray();
+        intStream.boxed().collect(Collectors.toList()); //collect into List of Integer
+        intStream.anyMatch(num -> num % 2 == 1);        //is any odd number?
+        intStream.allMatch(num -> num % 2 == 1);        //is all odd number?
+
+    }
+
+    @Test
+    public void streamCollectTest() {
+        //7. Collectors
+
+
+
+
+
     }
 }
